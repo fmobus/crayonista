025c2f5975fe79c199cfe43c6c6617abda4492e3
Felipe Mobus <fmobus@gmail.com>
2017-07-07 20:13:14 -0300

    initial commit using yeoman generator-react-webpack-redux

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.css';
+
+class AppComponent extends React.Component {
+
+  render() {
+    return (
+      <div className="index"/>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
